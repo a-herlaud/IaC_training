11aed443924d7add32ecce8b3109b3c28410ffe1
aherlaud <aherlaud@student.42.fr>
2026-08-18 15:18:02 +0200

    fix(typo error): fix typo error introduced at commiting stage

--- cluster_webserv/testing_cluster.sh
+++ cluster_webserv/testing_cluster.sh
@@ -7,5 +7,9 @@ terraform apply
 echo "TEST SERVER OUTPUT\n"
 
 terraform output -json public_ips | jq -r '.[]' | while read ip; do
-refactor
+  echo "━━━━━━━━━━━━━━━━━━━━━━━━━"
+  echo "Testing: http://$ip:8080"
+  echo "-------------------------"
+  curl "http://$ip:8080"
+  echo ""
 done
